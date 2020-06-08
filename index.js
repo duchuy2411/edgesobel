2c72d91dd490df37d10643392dc7b3a5d8e62f4c
unknown <bhanhmay31@gmail.com>
2020-06-08 15:36:29 +0700

    dhud

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+var bodyParser = require('body-parser');
+
+// Define the port to run on
+app.set('port', 8080);
+app.use('/public', express.static(path.join(__dirname, 'public')))
+
+var urlencodeParser = bodyParser.urlencoded({extended: false}); 
+
+app.set('view engine', 'ejs');
+app.set('views', '/views');
+
+app.use('/', require('./controller/router'));
+
+// Listen for requests  
+var server = app.listen(app.get('port'), function() {
+  var port = server.address().port;
+  console.log('Magic happens on port ' + port);
+});
+
