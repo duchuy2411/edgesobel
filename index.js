80b752ebcac3ad57bb50af4655bbee31a0d4c383
unknown <bhanhmay31@gmail.com>
2020-06-08 16:14:05 +0700

    lan4

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var bodyParser = require('body-parser');
 
 // Define the port to run on
 app.set('port', 8080);
-app.use('/public', express.static(path.join(__dirname, 'public')))
+app.use('', express.static(path.join(__dirname, '')));
 
 var urlencodeParser = bodyParser.urlencoded({extended: false}); 
 app.engine('html', require('ejs').renderFile);
